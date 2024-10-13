e38384854f757c32a6f8ec117befed15f11f58ba
ovolobuev <ovolobuev@mobiledep.ru>
2024-10-13 19:44:06 +0300

    [logger-front] сделал клиент для логгера

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,42 +1,159 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="page q-pa-md">
+    <div class="row q-mb-lg">
+      <q-input
+        v-model:model-value="filePath"
+        :rules="[ val => !!val || 'Это поле является обязательным' ]"
+        outlined
+        stack-label
+        clearable
+        style="width: 300px;"
+        label="Укажите путь к файлу"
+      />
+
+      <q-input
+        :model-value="amount"
+        outlined
+        stack-label
+        style="width: 300px;"
+        label="Укажите количество строк"
+        class="q-ml-md"
+        @update:model-value="updateAmount"
+      >
+        <template #prepend>
+          <q-btn
+            round
+            flat
+            icon="remove_circle"
+            @click="subAmount"
+          />
+        </template>
+
+        <template #append>
+          <q-btn
+            round
+            flat
+            icon="add_circle"
+            @click="addAmount"
+          />
+        </template>
+      </q-input>
+
+      <q-btn
+        :disable="!filePath"
+        style="height: 56px;"
+        color="primary"
+        class="q-ml-md"
+        label="Применить"
+        @click="apply"
+      />
+    </div>
+
+    <div>
+      <q-input
+        :model-value="logsList"
+        outlined
+        readonly
+        type="textarea"
+        class="output"
+      />
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
+import type { QInputProps } from 'quasar'
+import { ref, computed, onMounted } from 'vue'
+import { useQuasar } from 'quasar'
+import { api } from 'boot/axios'
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
+const q = useQuasar()
+
+const filePath = ref( 'e:/test/test.log' )
+const amount = ref( 4 )
+const logs = ref<Array<string>>([])
+
+const logsList = computed<string>( () => logs.value.join( '\n') )
+
+// положительные числа целые
+const REGEX_NUM_POS_INT = /^\d+$/;
+
+function updateAmount( value: QInputProps['modelValue'] ) {
+  if ( !value ) {
+    amount.value = 1
+    return
   }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+
+  if ( REGEX_NUM_POS_INT.test( value as string ) ) {
+    amount.value = value === '0' ? 1 : +value
+  }
+}
+
+function subAmount() {
+  if ( amount.value === 1 ) return
+  
+  amount.value--
+}
+
+function addAmount() {
+  amount.value++
+}
+
+async function apply() {
+  logs.value = []
+
+  try {
+    await api.post( 'http://localhost:5000/tail', {
+      // id: new Date().getTime(),
+      filePath: filePath.value,
+      nLines: amount.value
+    })
+  } catch ( e ) {
+    q.notify({
+      message: 'Ошибка',
+      caption: 'Неверно указан путь к файлу',
+      type: 'negative'
+    })
+  }
+}
+
+async function subscribe() {
+  const eventSource = new EventSource( 'http://localhost:5000/connect' )
+
+  eventSource.onmessage = function( event ) {
+    const data = JSON.parse( event.data )
+
+    logs.value.push( data )
+  }
+
+  // eventSource.onerror = function( event ) {
+  //   console.log( 'error:', event )
+  // }
+}
+
+onMounted( subscribe )
 </script>
+
+<style scoped lang="scss">
+.page {
+  max-height: calc(100vh - 50px);
+  display: grid;
+  grid-template-rows: max-content 1fr;
+}
+
+.output {
+  height: 100%;
+
+  :deep(.q-field__control) {
+    height: 100%;
+
+    &:before {
+      border-style: solid !important;
+    }
+
+    .q-field__native {
+      resize: none;
+    }
+  }
+}
+</style>
